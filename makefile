c88e50ecd7e5eefffc7eb20eae2bfaa7e0007645
Nikita Blinov <nblinov@gmail.com>
2020-01-02 00:06:34 -0600

    Initial committ

---
+++ makefile
@@ -0,0 +1,25 @@
+CC=g++
+CFLAGS=-O3 #-floop-parallelize-all -ftree-parallelize-loops=4 ##-fopenmp 
+DEFINES = -DUSE_OPEN_GL
+## for Mac OS X
+#GLFLAGS = -framework OpenGL -framework GLUT -lobjc
+## For Linux
+LDFLAGS = -L/usr/X11R6/lib
+GLFLAGS = -lglut -lGLU -lGL -lXmu -lXi -lXext -lX11 -lm 
+
+all: solar_openGL shell_clump spherical_collapse
+
+solar_openGL: solar_openGL.cpp particle.hpp vec2.hpp
+	$(CC) -o solar_openGL solar_openGL.cpp $(CFLAGS) $(DEFINES) $(LDFLAGS) $(GLFLAGS)
+
+shell_clump: shell_clump.cpp shell.hpp
+	$(CC) -o shell_clump shell_clump.cpp $(CFLAGS) $(DEFINES) $(LDFLAGS) $(GLFLAGS)
+
+spherical_collapse_open_GL: spherical_collapse.cpp shell.hpp
+	$(CC) -o spherical_collapse spherical_collapse.cpp $(CFLAGS) $(DEFINES) $(LDFLAGS)  $(GLFLAGS)
+
+spherical_collapse: spherical_collapse.cpp shell.hpp
+	$(CC) -o spherical_collapse spherical_collapse.cpp $(CFLAGS) $(LDFLAGS)  $(GLFLAGS)
+
+one_shell: one_shell.cpp shell.hpp
+	$(CC) -o one_shell one_shell.cpp $(CFLAGS) $(LDFLAGS)  $(GLFLAGS)
---
+++ shell.hpp
@@ -0,0 +1,58 @@
+#ifndef SHELL_HPP
+#define SHELL_HPP
+
+#include <iostream>
+using std::ostream;
+
+#include <cmath>
+using std::sqrt;
+
+class shell
+{
+public:
+	// member data
+	double mass;
+	double r;  // position
+	double vr; // radial velocity
+	double a;  // radial acceleration
+    double l;  // angular momentum
+	double t;  // current time;
+	double t_dyn;  // characteristic dynamical time;
+
+	// constructor
+	shell(double m_, double r_, double vr_, double l_) : mass(m_), r(r_), vr(vr_), a(0.), l(l_), t(0.) , t_dyn(0.) {}
+	shell(double m_, double r_, double vr_, double l_, double t_) : mass(m_), r(r_), vr(vr_), a(0.), l(l_), t(t_), t_dyn(0.) {}
+	
+	// methods (or member functions)
+	void evolve(void (&method)(shell&,double), double dt)
+	{
+		// *this refers to the active shell object
+		method(*this, dt);
+	}
+
+#ifdef USE_OPEN_GL
+	void draw(void)
+	{
+        const float radius = r;
+        glBegin(GL_LINE_LOOP); 
+          glColor3f(1.0, 0.5, 0.25); 
+          for(int j = 0; j < 360; ++j)
+            glVertex2f(radius*cos(j*M_PI/180.0f),radius*sin(j*M_PI/180.0f));
+        glEnd();
+		glFlush();
+	}
+#endif // USE_OPEN_GL	
+	
+	// a friend function does not belong to the class but is affiliated with it
+	friend ostream& operator<<(ostream& os, const shell &s)
+	{
+		os.precision(8);
+		os << setw(20) << s.t
+		    << setw(20) << s.r
+		    << setw(20) << s.vr
+		    << setw(20) << s.a;
+		return os;
+	}
+};
+
+#endif // SHELL_HPP
